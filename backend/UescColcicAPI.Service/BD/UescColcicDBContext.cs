7795600edb280550eb03e6d43715e3f854691006
Hélder Almeida <hcalmeida@uesc.br>
2024-09-18 17:32:48 -0300

    feat: Add UescColcicDBContext and StudentServices classes
    This commit adds the UescColcicDBContext class, which represents the database context for the UescColcicAPI service. It also includes the StudentServices class, which will handle operations related to students.
    
    The UescColcicDBContext class defines the Students DbSet and configures the primary key for the Student entity. It also configures the SQLite database provider for the context.
    
    The StudentServices class is currently empty and will be implemented in future commits.
    
    These changes are necessary to support the CRUD operations for students in the UescColcicAPI service.

---
+++ backend/UescColcicAPI.Service/BD/UescColcicDBContext.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using UescColcicAPI.Core;
+
+namespace UescColcicAPI.Services.BD;
+
+public class UescColcicDBContext : DbContext
+{
+   DbSet<Student> Students { get; set; }
+
+   protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+       modelBuilder.Entity<Student>().HasKey(x => x.StudentId);
+   }
+
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   {
+       optionsBuilder.UseSqlite();
+   }
+}
---
+++ backend/UescColcicAPI.Service/Services/StudentServices.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace UescColcicAPI.Services.Services;
+
+public class StudentServices
+{
+
+}
